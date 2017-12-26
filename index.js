d19a00e97bf1240599bb554eeb808873cdc60586
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-26 15:27:13 +0100

    display logo, name, id and online/offline status of array of streamers

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   for(var i=0; i < users.length; i++) {
     $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[i] + '?callback=?', function(data) {
       console.log(data);
+      $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + data._id + '?callback=?', function(datatwo) {
+        if(datatwo.stream === null) {
+          $('#printy').append('<img src="' + data.logo + '"/>' + " " + data.display_name + " " + data._id + " Offline" + '<br>');
+        } else {
+          $('#printy').append(data.display_name + " " + data._id + " Online" + '<br>');
+        }
+      });
     });
   }
 });
 
-//streams/summary
+
 
 const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
