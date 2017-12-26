ec3710b2b8ac934e61c5412dac203c3e29c60cbf
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-26 13:44:31 +0100

    use array of users instead of hard-coded value to log data in console

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-
-  $.getJSON('https://wind-bow.gomix.me/twitch-api/users/freecodecamp/?callback=?', function(data) {
-    console.log(data);
-  });
+  for(var i=0; i < users.length; i++) {
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + users[i] + '?callback=?', function(data) {
+      console.log(data);
+    });
+  }
 });
 
 //streams/summary
+
+const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
