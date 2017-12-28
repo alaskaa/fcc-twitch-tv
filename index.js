6c6ba5d1cafafb18ae8ae0f5fc89796b52cf4990
Sibylle Sehl <sibylle.sehl@googlemail.com>
2017-12-28 14:49:31 +0100

    update index.js and redo functionality and change basic design to cards

--- index.js
+++ index.js
@@ -7,41 +7,52 @@ for (var i = 0; i < users.length; i++) {
 
 
 function loadApi () {
-  let twitch_url = 'https://www.twitch.tv/' + users[i];
         $.ajax({
-            url: "https://wind-bow.gomix.me/twitch-api/channels/" + users[i] + "?callback=?",
+            url: "https://wind-bow.gomix.me/twitch-api/streams/" + users[i] + "?callback=?",
             dataType: "jsonp",
             data: {
                 format: "json"
             },
             success: function(data) {
-                $('#printy').append('<img src="' + data.logo + '"/>' + " " + '<a href="' + twitch_url + '" target="_blank">' + data.display_name + ' </a>' + data._id + '<br> <br>'),
                 ids = data._id;
                 console.log(data);
-                getStatus()
+                getStatus(data);
             }
         });
 }
 
-function getStatus() {
+function getStatus(data) {
+
+  if (data.stream === null) {
+    console.log("Streamer offline");
+    user_name = data._links.channel.substring(38);
+    getOfflineUsers();
+
+  }
+  else {
+    if(data.stream.channel.logo !== null) {
+      let url = (data.stream.channel.logo);
+      let twitch_url = "https://www.twitch.tv/" + data._links.channel.substring(38);
+      $('#printy').append('<div class="card"><img src="' + url + '"/>' + " " + '<a href="' + twitch_url + '" target="_blank">' + data._links.channel.substring(38) + ' </a>' + "is currently playing " + data.stream.game + '</div><br><br>');
+    }
+    else {
+      console.log("Error");
+    }
+  }
+}
+
+function getOfflineUsers() {
 
   $.ajax({
-      url: "https://wind-bow.gomix.me/twitch-api/streams/" +  + "?callback=?",
+      url: "https://wind-bow.gomix.me/twitch-api/users/" + user_name + "?callback=?",
       dataType: "jsonp",
       data: {
           format: "json"
       },
       success: function(data) {
-        //console.log(ids);
-        if(data.stream === null) {
-          $('#printytwo').append(ids + " Offline" + '<br> <br>');
-        }
-        else {
-          $('#printytwo').append(ids);
-        }
-      },
-      error: function () {
-          console.log("unable to access json");
+          console.log(data);
+          let twitch_url = "https://www.twitch.tv/" + data.display_name;
+          $('#printy').append('<div class="card"><img src="' + data.logo + '"/>' + " " + '<a href="' + twitch_url + '" target="_blank">' + data.display_name + ' </a>' + " is currently Offline " + '</div><br><br>');
       }
   });
 }
